017b8c67f05f5f627f6811689e14337c6f5e7be4
nickvholbrook <nick.holbrook@gmail.com>
2021-03-03 12:20:06 +0000

    First commit of a project within Intellij.

---
+++ src/garden/vegetable/VineVegetable.java
@@ -0,0 +1,7 @@
+package garden.vegetable;
+
+public class VineVegetable {
+    public static void main(String[] args) {
+        System.out.println("I'm a vegetable that grows on a vine");
+    }
+}
